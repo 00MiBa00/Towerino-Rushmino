be9d4bc9fe2178d82c2a398ddf32beb489ca2b01
Nikita <165208357+00MiBa00@users.noreply.github.com>
2026-03-06 13:08:26 +0100

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/services/sdk_initializer.dart';
@@ -10,11 +10,8 @@ import 'core/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initTrackingAppTransparency();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await Hive.initFlutter();
-  if (!Hive.isBoxOpen('tasks')) {
-    await Hive.openBox<String>('tasks');
-  }
   SdkInitializer.prefs = await SharedPreferences.getInstance();
   await SdkInitializer.loadRuntimeStorageToDevice();
   var isFirstStart = !SdkInitializer.hasValue("isFirstStart");
@@ -22,9 +19,31 @@ void main() async {
   if (kDebugMode) {
     print('add af2 $isFirstStart $isOrganic');
   }
+  if (isFirstStart) SdkInitializer.initAppsFlyer();
+
   runApp(
     const App(),
   );
+}
+
+Future<void> initTrackingAppTransparency() async {
+  try {
+    final TrackingStatus status =
+        await AppTrackingTransparency.requestTrackingAuthorization();
+    if (kDebugMode) {
+      print('App Tracking Transparency status: $status');
+    }
+    int timeout = 0;
+    while (status == TrackingStatus.notDetermined && timeout < 10) {
+      await AppTrackingTransparency.requestTrackingAuthorization();
+      await Future.delayed(const Duration(milliseconds: 200));
+      timeout++;
+    }
+  } catch (e) {
+    if (kDebugMode) {
+      print('Error requesting App Tracking Transparency authorization: $e');
+    }
+  }
 }
 
 class App extends StatelessWidget {
